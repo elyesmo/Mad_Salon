45b5405ab8bf4e2075e2a0c07adebb85aa2e2784
SAARAHK <96294062+SAARAHK@users.noreply.github.com>
2022-11-20 21:57:16 +0100

    Add files via upload

---
+++ php/est.php
@@ -0,0 +1,105 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="shortcut icon" href="#" />
+    <link rel="icon" href="../images/logo.png">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.0/css/all.css" />
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/styleHeadFoot.css">
+    <link rel="stylesheet" href="../css/styleLog.css">
+    <link rel="stylesheet" href="../css/est.css">
+    <!-- JavaScripts -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&display=swap" rel="stylesheet">
+    <title>Mad Salon - Login</title>
+</head>
+<body>
+<div class="header">
+    <nav class="navbar navbar-expand-lg navbar-dark  container">
+      <a class="navbar-brand" href="#">
+        <img class="lego" src="./images/logo.png" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./salon.php">Notre salon</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./realisations.php">Nos réalisations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./est.php">Esthétique</a>
+        </li>
+        </ul>
+        <a  href="./compte.php"><button type="button" class="btn btn-outline-light w-5"><i class="fas fa-user"></i> Compte</button></a>
+      </div>
+    </nav>
+  </div>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<h1 class="">Esthétique</h1>
+		</div>
+	</div>
+		<div class="row row-flex">
+			<div class="col-sm-6 image">
+                <img src="../images/masque-argile-concombre_shutterstock_474730186.webp" alt="soin"> 
+                    
+                          
+			</div>
+	<div class="col-sm-6 ">
+		<div class=" description">
+            Véritable source de bien-être et révélateur de style, le <strong>salon de coiffure Mad Salon</strong> propose également des formules intéressantes dans notre espace dédié aux soins esthétiques. <strong>Muriel</strong>, notre esthéticienne <strong>diplômée d’état</strong> révélera au grand jour la beauté naturelle de votre corps et votre visage en embellissant chaque partie.<br><br> Profitez des conseils de <strong>Muriel</strong> pour orienter votre sélection de produits cosmétiques adaptés à votre type de peau ou encore trouver la combinaison de maquillage qui saura mettre en valeur votre visage en toute occasion. 				</div>
+	    </div>
+    </div>
+	<div class="row">
+		<div class="description-bottom">
+		    <p>&nbsp;</p>
+
+		</div>
+	  </div>
+	</div>
+  <footer class=" text-center text-white">
+    <div class="container p-4 pb-0">
+      <section>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-twitter"></i
+        ></a>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-google"></i
+        ></a>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-instagram"></i
+        ></a>
+      </section>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2022 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MadSalon.com</a>
+    </div>
+    <script src="../js/compte.js"></script>
+  </footer>
+</body>
+</html>
