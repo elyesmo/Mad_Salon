5d17873ddc55377d4a3c6e36fd049ed78d1be46e
SAARAHK <96294062+SAARAHK@users.noreply.github.com>
2022-11-20 21:28:21 +0100

    Add files via upload

---
+++ php/salon.php
@@ -0,0 +1,120 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="shortcut icon" href="#" />
+    <link rel="icon" href="../images/logo.png">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.0/css/all.css" />
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/styleHeadFoot.css">
+    <link rel="stylesheet" href="../css/styleLog.css">
+    <link rel="stylesheet" href="../css/styleSalon.css">
+    <!-- JavaScripts -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&display=swap" rel="stylesheet">
+    <title>Mad Salon - Login</title>
+</head>
+<body>
+<div class="header">
+    <nav class="navbar navbar-expand-lg navbar-dark  container">
+      <a class="navbar-brand" href="#">
+        <img class="lego" src="./images/logo.png" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./salon.php">Notre salon</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./realisations.php">Nos réalisations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./est.php">Esthétique</a>
+        </li>
+        </ul>
+        <a  href="./compte.php"><button type="button" class="btn btn-outline-light w-5"><i class="fas fa-user"></i> Compte</button></a>
+      </div>
+    </nav>
+  </div>
+
+
+
+
+
+
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+				<h1 class="">Notre salon</h1>
+		</div>
+	</div>
+<div class="row row-flex">
+  <div class="col-sm-6 image">
+     <img src="../images/coifella-salon2.jpeg" alt="salon">   
+  </div>
+
+	<div class="col-sm-6 ">
+		<div class=" description">
+							Votre <strong>salon de coiffure Mad Salon </strong>et son équipe de professionnels&nbsp;<strong>diplômés d’état</strong> situé à <strong>Paris</strong> vous accueille dans un <strong>cadre moderne</strong> et convivial propice à la détente et à l’évasion.
+Nos <strong>7 espaces de coiffage</strong> disposent d’équipements complets et performants qui permettront à nos <strong>trois coiffeurs</strong> professionnels de réaliser des prouesses sur votre chevelure.
+Depuis <strong>13 ans</strong>, nos experts donnent le meilleur d’eux-mêmes pour sublimer votre chevelure. Plus qu’un simple <strong>salon de coiffure</strong>, nos stylistes vous orienteront vers des choix adaptés à votre morphologie et votre personnalité : <strong>coupe</strong>, <strong>balayage</strong> ou encore <strong>coloration</strong>.
+De plus, votre séance chez <strong>Mad Salon</strong> sera accompagnée de conseils judicieux qui vous permettront d’entretenir efficacement votre chevelure.
+Pour un <strong>relooking</strong> réussi, <strong>Mad Salon</strong>reste LA référence pour la <strong>recherche d’un style capillaire tendance</strong>, effectuée par une <strong>équipe proche de sa clientèle</strong>&nbsp;et qui <strong>saura analyser vos envies avant de passer à la coupe</strong>.						</div>
+		</div>
+	</div>
+		<div class="row">
+				<div class="description-bottom">
+					<p>&nbsp;</p>
+
+				</div>
+			</div>
+	</div>
+
+  <footer class=" text-center text-white">
+    <div class="container p-4 pb-0">
+      <section>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-twitter"></i
+        ></a>
+
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-google"></i
+        ></a>
+
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-instagram"></i
+        ></a>
+      </section>
+    </div>
+
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2022 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MadSalon.com</a>
+    </div>
+  </footer>
+
+  <!-- <script src="./js/main.js"></script> -->
+</body>
+</html>
